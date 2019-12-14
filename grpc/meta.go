875f90c1a4be93b3c4b6323dda711fecd0180a11
Simo Ala-Kotila <simo.alakotila@gmail.com>
2019-12-14 19:11:25 +0200

    added meta keys

---
+++ grpc/meta.go
@@ -0,0 +1,8 @@
+package grpc
+
+const (
+	// usefully when you don't wanna duplicates for example username and etc..
+	Unique 		= "unique"
+	// something entity did not exist for example user
+	NotFound 	= "not_found"
+)
