5f669c2314e79c1488f9f48085819629761edaf1
Simo Ala-Kotila <simo.alakotila@gmail.com>
2019-12-14 16:48:51 +0200

    added mysql constats

--- sqlz/misc.go
+++ sqlz/misc.go
@@ -2,6 +2,14 @@ package sqlz
 
 import "fmt"
 
+const (
+	MysqlRootPassword = "MYSQL_ROOT_PASSWORD"
+	MysqlDatabase     = "MYSQL_DATABASE"
+	MysqlUser         = "MYSQL_USER"
+	MysqlHost         = "MYSQL_HOST"
+	MysqlPort         = "MYSQL_PORT"
+)
+
 //db_user:password@tcp(localhost:3306)/my_db
 //root:root@(localhost:3306)/shopping_list
 //<username>:<pw>@tcp(<HOST>:<port>)/<dbname>
