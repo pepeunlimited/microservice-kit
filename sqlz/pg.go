6e5d09a48589a44634f824ee0487e43cad2b0626
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-03-17 23:31:31 +0200

    added postgresql

---
+++ sqlz/pg.go
@@ -0,0 +1,23 @@
+package sqlz
+
+import "fmt"
+
+const (
+	PostgresqlPassword 	   = "POSTGRESQL_PASSWORD"
+	PostgresqlDatabase     = "POSTGRESQL_DATABASE"
+	PostgresqlUser         = "POSTGRESQL_USER"
+	PostgresqlHost         = "POSTGRESQL_HOST"
+	PostgresqlPort         = "POSTGRESQL_PORT"
+	PostgresqlSslMode      = "POSTGRESQL_SSL_MODE"
+)
+
+//host=%s port=%d user=%s password=%s dbname=%s sslmode=%s
+func PostgreSQLURI(user string, pw string, host string, port int, database string, sslmode bool) string {
+	var sslmodetext string
+	if sslmode {
+		sslmodetext = "enabled"
+	} else {
+		sslmodetext = "disabled"
+	}
+	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s", host, port, user, database, pw, sslmodetext)
+}
---
+++ sqlz/pg_test.go
@@ -0,0 +1,7 @@
+package sqlz
+
+import "testing"
+
+func TestPostgreSQLURI(t *testing.T) {
+	println(PostgreSQLURI("root", "root", "localhost", 5432, "jemmadb",false))
+}
