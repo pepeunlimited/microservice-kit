ba2cc6709fa60df8e68fec67f2472acfa281597f
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-05-08 00:50:40 +0300

    added few const

--- email/const.go
+++ email/const.go
@@ -5,4 +5,7 @@ const (
 	PiiiaCom = "info@piiia.com"
 	SmtpUsername = "SMTP_USERNAME"
 	SmtpPassword = "SMTP_PASSWORD"
+	SmtpFrom 	      	   = "SMTP_FROM"
+	SmtpFromName 	  	   = "SMTP_FROM_NAME"
+	SmtpClient			   = "SMTP_CLIENT"
 )
