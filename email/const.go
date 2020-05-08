8c056195f68cd2dbe5c96c4461bafc9637c82eca
Simo Ala-Kotila <simo.alakotila@gmail.com>
2020-05-08 10:10:33 +0300

    ClientMode

--- email/const.go
+++ email/const.go
@@ -1,11 +1,11 @@
 package email
 
 const (
-	PepeUnlimited = "info@pepeunlimited.com"
-	PiiiaCom = "info@piiia.com"
-	SmtpUsername = "SMTP_USERNAME"
-	SmtpPassword = "SMTP_PASSWORD"
-	SmtpFrom 	      	   = "SMTP_FROM"
-	SmtpFromName 	  	   = "SMTP_FROM_NAME"
-	SmtpClient			   = "SMTP_CLIENT"
+	PepeUnlimited 	= "info@pepeunlimited.com"
+	PiiiaCom 		= "info@piiia.com"
+	SmtpUsername 	= "SMTP_USERNAME"
+	SmtpPassword 	= "SMTP_PASSWORD"
+	SmtpFrom 	 	= "SMTP_FROM"
+	SmtpFromName 	= "SMTP_FROM_NAME"
+	SmtpClientMode	= "SMTP_CLIENT_MODE"
 )
