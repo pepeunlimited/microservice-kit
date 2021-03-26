423bd9e1a09a4570ccbe407c3dfe0a296d27a2cb
Simo Ala-Kotila <simo.alakotila@gmail.com>
2021-03-26 13:19:34 +0200

    bump

--- sqlz/pg.go
+++ sqlz/pg.go
@@ -12,12 +12,20 @@ const (
 )
 
 //host=%s port=%d user=%s password=%s dbname=%s sslmode=%s
-func PostgreSQLURI(user string, pw string, host string, port int, database string, sslmode bool) string {
+func PostgreSQLURI(user string, pw string, host string, port int, database string, sslmode bool, binaryparameters bool) string {
 	var sslmodetext string
 	if sslmode {
 		sslmodetext = "require"
 	} else {
 		sslmodetext = "disable"
 	}
-	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s", host, port, user, database, pw, sslmodetext)
+
+	var binaryparameterstext string
+	if binaryparameters {
+		binaryparameterstext = "yes"
+	} else {
+		binaryparameterstext = "no"
+	}
+
+	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s binary_parameters=%s", host, port, user, database, pw, sslmodetext, binaryparameterstext)
 }
--- sqlz/pg_test.go
+++ sqlz/pg_test.go
@@ -3,5 +3,5 @@ package sqlz
 import "testing"
 
 func TestPostgreSQLURI(t *testing.T) {
-	println(PostgreSQLURI("root", "root", "localhost", 5432, "jemmadb",false))
+	println(PostgreSQLURI("root", "root", "localhost", 5432, "jemmadb",false, true))
 }
